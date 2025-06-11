0d2304f1c44222f9e9152e424aab1fae3b3a6831
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2025-06-11 16:15:59 +0000

    0x06. Unittests in JS

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,30 +1,13 @@
-/* eslint-disable jest/expect-expect */
-/* eslint-disable jest/valid-expect */
-const { expect } = require('chai');
-const assert = require('assert');
+const getPaymentTokenFromAPI = require("./6-payment_token");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
 
-const getPaymentTokenFromAPI = require('./6-payment_token');
-
-describe('getPaymentTokenFromAPI', () => {
-  it('returns a resolved promise with the object {data: "Successful response from the API"} when success is true', () => new Promise((done) => {
-    getPaymentTokenFromAPI(true)
-      .then((response) => {
-        expect(response).to.deep.equal({ data: 'Successful response from the API' });
-        done();
-      })
-      .catch(done);
-  }));
-
-  // FAILING BELOW
-  // eslint-disable-next-line jest/no-commented-out-tests, jest/no-disabled-tests
-  it.skip('should return a rejected promise when success is false', () => new Promise((done) => {
-    getPaymentTokenFromAPI(false, (err) => {
-      try {
-        assert.strictEqual(err, 'Error');
-        done();
-      } catch (err) {
-        done(err);
-      }
+describe("getPaymentTokenFromAPI", function() {
+    it("Async testing with done callback", function(done) {
+	getPaymentTokenFromAPI(true)
+	    .then((data) => {
+		expect(data).to.have.property('data');
+		done();
+	    });
     });
-  }));
 });
